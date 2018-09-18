0877cf5ffbc463c911f40a6878f6df196111bffb
Geass <dmjliar@gmail.com>
2018-09-18 19:03:37 +0800

    finish Third

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -1,14 +1,20 @@
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# Language QuasiQuotes       #-}
+{-# LANGUAGE RankNTypes        #-}
 {-# LANGUAGE TemplateHaskell   #-}
 {-# LANGUAGE TypeFamilies      #-}
-{-# Language QuasiQuotes       #-}
 
 module Foundation where
 
+import ClassyPrelude
+import ClassyPrelude.Yesod
 import Database.Persist.Sql
 import Network.HTTP.Client
+import Text.Hamlet
 import Yesod.Core
 import Yesod.Core.Types
+import Yesod.Form
 import Yesod.Static
 
 import Settings
@@ -27,9 +33,25 @@ mkYesodData
     /                        HomeR               GET
   |]
 
+type Form a = Html -> MForm (HandlerFor App) (FormResult a, Widget)
+type DB a = forall (m:: * -> *). (MonadIO m) => ReaderT SqlBackend m a
 
 instance Yesod App where
     approot = ApprootRequest $ \app req ->
         case appRoot $ appSettings app of
             Nothing -> getApprootText guessApproot app req
             Just root -> root
+    yesodMiddleware = defaultYesodMiddleware
+    defaultLayout widget = do
+        master <- getYesod
+        mmessage <- getMessage
+        mcurrentroute <- getCurrentRoute
+        pagecontent <- widgetToPageContent $ do
+            [whamlet|
+                $maybe rout <- mcurrentroute
+                    <p>You're at ${show route}.
+                $nothing
+                    <p>You're lost.
+                ^{widget}
+            |]
+        withUrlRenderer $(hamletFile "templates/wrapper.hamlet")
