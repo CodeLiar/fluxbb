ce74231d414b93072aedb726d027c696d956b45f
Geass <dmjliar@gmail.com>
2018-09-21 14:08:45 +0800

    administer user. promote.

--- src/Flux/Miscellaneous.hs
+++ src/Flux/Miscellaneous.hs
@@ -19,3 +19,33 @@ getAllGroups ::
   => m [Entity Groups]
 getAllGroups = liftHandler $ runDB $ selectAllGroups
 
+getGroup ::
+     ( BackendCompatible SqlBackend (YesodPersistBackend (HandlerSite m))
+     , PersistQueryRead (YesodPersistBackend (HandlerSite m))
+     , PersistUniqueRead (YesodPersistBackend (HandlerSite m))
+     , YesodPersist (HandlerSite m)
+     , MonadHandler m
+     )
+  => Grouping
+  -> m (Entity Groups)
+getGroup x = do
+  a <- liftHandler . runDB . selectGroupByGrouping $ x
+  case a of
+    [g] -> return g
+    _   -> invalidArgs ["Group not found."]
+
+getGroupById ::
+     ( BackendCompatible SqlBackend (YesodPersistBackend (HandlerSite m))
+     , PersistQueryRead (YesodPersistBackend (HandlerSite m))
+     , PersistUniqueRead (YesodPersistBackend (HandlerSite m))
+     , YesodPersist (HandlerSite m)
+     , MonadHandler m
+     )
+  => Key Groups
+  -> m (Entity Groups)
+getGroupById gid = do
+  a <- liftHandler . runDB . selectGroupByGroupId $ gid
+  case a of
+    [g] -> return g
+    _   -> invalidArgs ["Group not found."]
+
