a2084e4e013dbeac34fb8e8adb522f0bf0a1a992
Geass <dmjliar@gmail.com>
2018-09-21 10:29:11 +0800

    home

---
+++ src/Import/Util.hs
@@ -0,0 +1,12 @@
+module Import.Util
+  ( forceTextToInt64
+  ) where
+
+import           ClassyPrelude.Yesod
+
+forceTextToInt64 :: Text -> Int64
+forceTextToInt64 t =
+  case readMay t of
+    Just i  -> i :: Int64
+    Nothing -> 0
+
